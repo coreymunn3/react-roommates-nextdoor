44f66065b17f1814802692085179e8c6ffe1f378
Mo Omane <momane@cainc.com>
2021-02-19 17:34:24 -0500

    initial build for post modal

--- client/src/post-component/create/index.js
+++ client/src/post-component/create/index.js
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
-
-function CreatePost() {
-  return <div></div>;
-}
+import React from "react";
+import Modal from "react-modal";
+const CreatePost = (active) => {
+  return (
+    <div>
+      <Modal isOpen={active}>
+        <h2>Create Post</h2>
+        <p>Modal Body</p>
+        <button onClick={}></button>
+      </Modal>
+    </div>
+  );
+};
 
 export default CreatePost;
--- client/src/post-component/index.js
+++ client/src/post-component/index.js
@@ -1,12 +1,24 @@
 // Post Creation Component
 import React, { useState } from "react";
 import Create from "./create/index";
+import ReactDOM from "react-dom";
 
 function PostCreateButton() {
-  const displayPostCreation = () => {};
+  const [modalOpen, setModal] = useState(false);
+
+  const displayCreatePost = () => {
+    setModal(true);
+    ReactDOM.render(
+      <Create active={modalOpen}> </Create>,
+      document.getElementById("root")
+    );
+  };
   return (
     <React.Fragment>
-      <button className="create_post_button" onClick={displayPostCreation}>
+      <button
+        className="create_post_button"
+        onClick={() => displayCreatePost()}
+      >
         Create Post
       </button>
     </React.Fragment>
