5240d82f9d86f914864955cc2e50c3c484ebf348
Michael Corey Munn <coreymunn3@gmail.com>
2021-03-28 16:30:11 -0400

    Added down caret to feed controls to be used as toggle for eventual accordion.

--- client/src/components/routes/feed/Feed.js
+++ client/src/components/routes/feed/Feed.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import FeedControls from '../../feedControls/FeedControls';
+import { FaSortDown } from 'react-icons/fa';
 // redux
 import { useSelector } from 'react-redux';
+// styles
+import styles from './feed.module.scss';
 
 const Feed = () => {
   const { user, isLoading } = useSelector((state) => state.user);
@@ -12,7 +15,15 @@ const Feed = () => {
         <h3>Loading</h3>
       ) : (
         <div>
-          <h3>{`Recent Posts In ${user?.user?._location?.city}, ${user?.user?._location?.state}`}</h3>
+          <div className={styles.titleContainer}>
+            <h3>{`Recent Posts In ${user?.user?._location?.city}, ${user?.user?._location?.state}`}</h3>
+            <FaSortDown
+              size='2em'
+              className={styles.feedControlToggle}
+              onClick={() => console.log('open controls')}
+            />
+          </div>
+
           <FeedControls />
           {/* posts */}
         </div>
