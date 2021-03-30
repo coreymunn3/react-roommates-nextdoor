da522ee00462304949b04fabd960bc324fbd32f6
Michael Corey Munn <coreymunn3@gmail.com>
2021-03-30 10:11:14 -0400

    Fixed up PostCard, added media queries, badges. Added message in PostCardContainer if there are no post yet. Installed moment to use .fromNow method, may remove later

--- client/src/components/postCard/PostCard.js
+++ client/src/components/postCard/PostCard.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import moment from 'moment';
 import styles from './postCard.module.scss';
 
-const PostCard = ({ post: { title, body, featureImage } }) => {
+const PostCard = ({
+  post: {
+    title,
+    body,
+    featureImage,
+    rentMonthly,
+    securityDeposit,
+    totalMoveInCost,
+    datePosted,
+    numberOfCohabitants,
+  },
+}) => {
   return (
-    <div className='my-2'>
-      <Card className='shadow'>
+    <div className='my-3'>
+      <Card className='shadow-sm'>
         <Card.Img src={featureImage} alt='featureImage' />
         {/* <Card.ImgOverlay>
           <Card.Title>{title}</Card.Title>
@@ -14,12 +27,28 @@ const PostCard = ({ post: { title, body, featureImage } }) => {
           <Card.Text>Last updated 3 mins ago</Card.Text>
         </Card.ImgOverlay> */}
         <Card.Body>
+          <div className={styles.badges}>
+            <Badge
+              variant='primary'
+              className='mr-1'
+            >{`Rent $${rentMonthly}`}</Badge>
+            <Badge
+              variant='primary'
+              className='mr-1'
+            >{`Move In $${totalMoveInCost}`}</Badge>
+            <Badge
+              variant='secondary'
+              className='mr-1'
+            >{`${numberOfCohabitants} Other Person`}</Badge>
+          </div>
           <div className={styles.cardTitle}>
             <h4 className='my-0'>{title}</h4>
-            <small className='text-muted'>4 Days ago</small>
+            <small className='text-muted'>{moment(datePosted).fromNow()}</small>
           </div>
           <Card.Text>{body}</Card.Text>
-          <Button variant='success'>See Full Post</Button>
+          <Button variant='success' className='shadow-sm'>
+            See Full Post
+          </Button>
         </Card.Body>
       </Card>
     </div>
--- client/src/components/postsContainer/PostsContainer.js
+++ client/src/components/postsContainer/PostsContainer.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import PostCard from '../postCard/PostCard';
+import styles from './postsContainer.module.scss';
 
 const PostsContainer = () => {
   const { feedPosts, isLoading: postsLoading } = useSelector(
     (state) => state.post
   );
+  const noPostsYet = feedPosts.length === 0;
 
   return (
     <div className='my-2'>
+      {noPostsYet && (
+        <div className={styles.noPostsContainer}>
+          <h6 className='text-muted'>There Aren't Any Posts Here Yet!</h6>
+        </div>
+      )}
       {feedPosts.map((post, idx) => (
         <PostCard key={idx} post={post} />
       ))}
