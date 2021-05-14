94235f6070e10449fc76cdd536d5d968d0276f06
Michael Corey Munn <coreymunn3@gmail.com>
2021-05-14 13:01:35 -0400

    added layout to notFound component

--- client/src/components/routes/notFound/NotFound.js
+++ client/src/components/routes/notFound/NotFound.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import FullHeightContainer from '../../layout/fullHeightContainer/FullHeightContainer';
+import PageLayout from '../../layout/PageLayout';
 
 const NotFound = () => {
   return (
-    <FullHeightContainer>
-      <h1>404 | Page Not Found</h1>
-    </FullHeightContainer>
+    <PageLayout>
+      <FullHeightContainer>
+        <h1>404 | Page Not Found</h1>
+      </FullHeightContainer>
+    </PageLayout>
   );
 };
 
