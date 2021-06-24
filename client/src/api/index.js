8e56ce7cc1aeabe75b0b515b7abd5821f030501f
Michael Corey Munn <coreymunn3@gmail.com>
2021-06-23 21:48:58 -0400

    changed API baseURL for netlify deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:5000';
+const baseUrl = 'https://roommates-project-server.herokuapp.com/';
 
 export const userAPI = {
   signup: (newUserData) =>
